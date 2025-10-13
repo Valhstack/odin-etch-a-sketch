6a115fb5623e53b79a0bad9bb64252dc2db14fca
Valeriia <43146435+ValeriiaHolovatiuk@users.noreply.github.com>
2025-10-13 22:57:38 +0200

    Added the option to color in different opacity

--- scripts/hoveringListener.js
+++ scripts/hoveringListener.js
@@ -1,15 +1,25 @@
 const sketchGrid = document.getElementById("sketchGrid");
 let hoverEnabled = false;
+let opacity = 0.1;
 
 sketchGrid.addEventListener("mouseover", (event) => {
+    let currentOpacity = parseFloat(event.target.style.opacity) || 0;
+    let newOpacity = currentOpacity + 0.1;
+
+    if (newOpacity > 1) {
+        newOpacity = 1;
+    }
+
     if (!colorPicked) {
         color = "blue";
     }
-    
+
     if (hoverEnabled && !eraserEnabled) {
         event.target.style.backgroundColor = color;
+        event.target.style.opacity = newOpacity;
     }
     else if (hoverEnabled && eraserEnabled) {
         event.target.style.backgroundColor = "#2c3e50";
+        event.target.style.opacity = "1";
     }
 })
