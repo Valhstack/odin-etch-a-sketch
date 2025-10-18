6664e3bcf14ce0519c3e8a28519127e9bdb988d6
Valeriia <43146435+ValeriiaHolovatiuk@users.noreply.github.com>
2025-10-18 22:41:35 +0200

    Added two additional handlers to support touch screen

--- scripts/hoveringListener.js
+++ scripts/hoveringListener.js
@@ -1,5 +1,5 @@
 const sketchGrid = document.getElementById("sketchGrid");
-let hoverEnabled = false;
+let hoverEnabled = false, touchDrawing = false;
 let opacity = 0.1;
 
 function changeOpacity() {
@@ -12,7 +12,7 @@ function changeOpacity() {
 }
 
 function handleMove(event) {
-    
+
     changeOpacity();
 
     if (!colorPicked) {
@@ -29,21 +29,35 @@ function handleMove(event) {
     }
 }
 
+sketchGrid.addEventListener("pointerdown", (event) => {
+    if (event.pointerType === "touch") {
+        touchDrawing = true;
+    }
+})
+
 sketchGrid.addEventListener("pointermove", handleMove);
 
 sketchGrid.addEventListener("pointerover", (event) => {
-    changeOpacity();
+    if (touchDrawing) {
+        changeOpacity();
 
-    if (!colorPicked) {
-        color = "blue";
-    }
+        if (!colorPicked) {
+            color = "red";
+        }
 
-    if (hoverEnabled && !eraserEnabled) {
-        event.target.style.backgroundColor = color;
-        event.target.style.opacity = newOpacity;
+        if (hoverEnabled && !eraserEnabled) {
+            event.target.style.backgroundColor = color;
+            event.target.style.opacity = newOpacity;
+        }
+        else if (hoverEnabled && eraserEnabled) {
+            event.target.style.backgroundColor = "#2c3e50";
+            event.target.style.opacity = "1";
+        }
     }
-    else if (hoverEnabled && eraserEnabled) {
-        event.target.style.backgroundColor = "#2c3e50";
-        event.target.style.opacity = "1";
+})
+
+sketchGrid.addEventListener('pointerup', (event) => {
+    if (event.pointerType === "touch") {
+        touchDrawing = false;
     }
 })
