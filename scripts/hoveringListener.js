1c0439ea247f5df606fd50fbc990c4a3015529bb
Valeriia <43146435+ValeriiaHolovatiuk@users.noreply.github.com>
2025-10-18 22:06:14 +0200

    Attempt one to add mobile support

--- scripts/hoveringListener.js
+++ scripts/hoveringListener.js
@@ -2,13 +2,36 @@ const sketchGrid = document.getElementById("sketchGrid");
 let hoverEnabled = false;
 let opacity = 0.1;
 
-sketchGrid.addEventListener("pointerover", (event) => {
+function changeOpacity() {
     let currentOpacity = parseFloat(event.target.style.opacity) || 0;
     let newOpacity = currentOpacity + 0.1;
 
     if (newOpacity > 1) {
         newOpacity = 1;
     }
+}
+
+function handleMove(event) {
+    changeOpacity();
+
+    if (!colorPicked) {
+        color = "blue";
+    }
+
+    if (hoverEnabled && !eraserEnabled) {
+        event.target.style.backgroundColor = color;
+        event.target.style.opacity = newOpacity;
+    }
+    else if (hoverEnabled && eraserEnabled) {
+        event.target.style.backgroundColor = "#2c3e50";
+        event.target.style.opacity = "1";
+    }
+}
+
+sketchGrid.addEventListener("pointermove", handleMove);
+
+sketchGrid.addEventListener("pointerover", (event) => {
+    changeOpacity();
 
     if (!colorPicked) {
         color = "blue";
