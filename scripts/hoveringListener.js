31af8029bbfc591f2f68cc685d611614bc9620a0
Valeriia <43146435+ValeriiaHolovatiuk@users.noreply.github.com>
2025-10-21 22:22:35 +0200

    Checking pointer coordinates

--- scripts/hoveringListener.js
+++ scripts/hoveringListener.js
@@ -11,58 +11,6 @@ function changeOpacity() {
     }
 }
 
-/*function handleMove(event) {
-    changeOpacity();
-
-    if (!colorPicked) {
-        color = "red";
-    }
-
-    if (hoverEnabled && !eraserEnabled) {
-        event.target.style.backgroundColor = color;
-        event.target.style.opacity = newOpacity;
-    }
-    else if (hoverEnabled && eraserEnabled) {
-        event.target.style.backgroundColor = "#2c3e50";
-        event.target.style.opacity = "1";
-    }
-}*/
-
-sketchGrid.addEventListener("pointerdown", (event) => {
-    if (event.pointerType === "touch") {
-        sketchGrid.setPointerCapture(event.pointerId);
-
-        touchDrawing = true;
-    }
-
-    /*sketchGrid.onpointermove = function (event) {
-        if (!event.target.classList.contains("square")) return;
-
-        changeOpacity();
-
-        if (!colorPicked) {
-            color = "purple";
-        }
-
-        if (hoverEnabled && !eraserEnabled) {
-            event.target.style.backgroundColor = color;
-            event.target.style.opacity = newOpacity;
-        }
-        else if (hoverEnabled && eraserEnabled) {
-            event.target.style.backgroundColor = "#2c3e50";
-            event.target.style.opacity = "1";
-        }
-    };
-
-    sketchGrid.onpointerup = function (event) {
-        if (event.pointerType === "touch") {
-            touchDrawing = false;
-        }
-    }*/
-})
-
-//sketchGrid.addEventListener("pointermove", handleMove);
-
 function colorSquare(square) {
     changeOpacity();
 
@@ -80,26 +28,11 @@ function colorSquare(square) {
     }
 }
 
-sketchGrid.addEventListener("pointerover", (event) => {
-
-    if(event.target.classList.contains("square")) colorSquare(event.target);
-    
-    /*changeOpacity();
-
-    if (!colorPicked) {
-        color = "green";
-    }
-
+sketchGrid.addEventListener("pointermove", (event) => {
     if (event.target.classList.contains("square")) {
-        if (hoverEnabled && !eraserEnabled) {
-            event.target.style.backgroundColor = color;
-            event.target.style.opacity = newOpacity;
-        }
-        else if (hoverEnabled && eraserEnabled) {
-            event.target.style.backgroundColor = "#2c3e50";
-            event.target.style.opacity = "1";
-        }
-    }*/
+        colorSquare(event.target);
+        console.log("X: ", event.clientX, " Y: ", event.clientY);
+    }
 })
 
 sketchGrid.addEventListener('pointerup', (event) => {
